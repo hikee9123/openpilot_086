348b8e2b594b07b2ccbd7342ec19fe761a535157
hikee9123 <hikee9123@gmail.com>
2021-03-24 20:53:31 +0900

    dd

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -254,8 +254,8 @@ static void update_sockets(UIState *s) {
 //#ifdef QCOM2
   if (sm.updated("roadCameraState")) {
     scene.camera_state = sm["roadCameraState"].getRoadCameraState();
-    float gain = camera_state.getGainFrac() * (camera_state.getGlobalGain() > 100 ? 2.5 : 1.0) / 10.0;
-    scene.light_sensor = std::clamp<float>((1023.0 / 1757.0) * (1757.0 - camera_state.getIntegLines()) * (1.0 - gain), 0.0, 1023.0);
+    float gain = scene.camera_state.getGainFrac() * (scene.camera_state.getGlobalGain() > 100 ? 2.5 : 1.0) / 10.0;
+    scene.light_sensor = std::clamp<float>((1023.0 / 1757.0) * (1757.0 - scene.camera_state.getIntegLines()) * (1.0 - gain), 0.0, 1023.0);
   }
 //#endif
   scene.started = scene.deviceState.getStarted() || scene.driver_view;
