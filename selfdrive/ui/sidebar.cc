8ddf642165abe85875208dd725e89be57571777a
hikee9123 <hikee9123@gmail.com>
2021-03-26 22:41:51 +0900

    dd

--- selfdrive/ui/sidebar.cc
+++ selfdrive/ui/sidebar.cc
@@ -117,7 +117,7 @@ static void draw_temp_metric(UIState *s) {
       {cereal::DeviceState::ThermalStatus::YELLOW, 1},
       {cereal::DeviceState::ThermalStatus::RED, 2},
       {cereal::DeviceState::ThermalStatus::DANGER, 3}};
-  std::string temp_val = std::to_string((int)s->scene.deviceState.getAmbientTempC()) + "¡ÆC";
+  std::string temp_val = std::to_string((int)s->scene.deviceState.getAmbientTempC()) + "C";
   draw_metric(s, "TEMP", temp_val.c_str(), temp_severity_map[s->scene.deviceState.getThermalStatus()], 0, NULL);
 }
 
