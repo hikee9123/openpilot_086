3df17d5a745976788dcd985283941537ca6f1362
hikee9123 <hikee9123@gmail.com>
2021-03-28 19:14:56 +0900

    dd

--- selfdrive/car/tests/test_car_interfaces.py
+++ selfdrive/car/tests/test_car_interfaces.py
@@ -24,12 +24,12 @@ class TestSpdCtrl:
       #model = self.sm['modelV2']
       #lane_lines = model.laneLines
       #lane_line_probs = model.laneLineProbs
-
-      curve_speed = self.SC.cal_curve_speed(  30, 10  )
-      model_speed = self.SC.cal_model_speed(   30  )
-      #self.prob = list(model.path.poly)
-      #model_speed = self.SC.calc_laneProb(  self.prob, 30  )
-      print( 'model_speed = {}  curve_speed={} '.format( model_speed, curve_speed) )
+      while True:
+        curve_speed = self.SC.cal_curve_speed(  30, 10  )
+        model_speed = self.SC.cal_model_speed(   30  )
+        #self.prob = list(model.path.poly)
+        #model_speed = self.SC.calc_laneProb(  self.prob, 30  )
+        print( 'model_speed = {}  curve_speed={} '.format( model_speed, curve_speed) )
 
 
 
