dd3f5f6c551a39e1347154545c2e314f48717a15
hikee9123 <hikee9123@gmail.com>
2021-03-28 12:50:01 +0900

    dd

--- selfdrive/ui/qt/atom/userPanel.cc
+++ selfdrive/ui/qt/atom/userPanel.cc
@@ -10,49 +10,13 @@
 #include "widgets/toggle.hpp"
 #include "widgets/offroad_alerts.hpp"
 #include "widgets/controls.hpp"
-#include "widgets/ssh_keys.hpp"
+
 #include "common/params.h"
 #include "common/util.h"
 
 
 #include "userPanel.hpp"
 
-/*
-UserPanel::UserPanel(QWidget* parent) : QFrame(parent)
-{
-  QVBoxLayout *layout = new QVBoxLayout;
-  layout->setMargin(100);
-  layout->setSpacing(30);
-
-  // simple wifi + tethering buttons
-  const char* launch_wifi = "am start -n com.android.settings/.wifi.WifiPickerActivity \
-                             -a android.net.wifi.PICK_WIFI_NETWORK \
-                             --ez extra_prefs_show_button_bar true \
-                             --es extra_prefs_set_next_text ''";
-  layout->addWidget(new ButtonControl("WiFi Settings", "OPEN", "",
-                                      [=]() { std::system(launch_wifi); }));
-
-  layout->addWidget(horizontal_line());
-
-  const char* launch_tethering = "am start -n com.android.settings/.TetherSettings \
-                                  --ez extra_prefs_show_button_bar true \
-                                  --es extra_prefs_set_next_text ''";
-  layout->addWidget(new ButtonControl("Tethering Settings", "OPEN", "",
-                                      [=]() { std::system(launch_tethering); }));
-
-  layout->addWidget(horizontal_line());
-
-  // SSH key management
-  layout->addWidget(new SshToggle());
-  layout->addWidget(horizontal_line());
-  layout->addWidget(new SshControl());
-
-  layout->addStretch(1);
-
-  QWidget *w = new QWidget;
-  w->setLayout(layout);
-}
-*/
 
 UserPanel::UserPanel(QWidget* parent) : QFrame(parent) 
 {
@@ -61,38 +25,23 @@ UserPanel::UserPanel(QWidget* parent) : QFrame(parent)
   setLayout(main_layout);
  // setStyleSheet(R"(QLabel {font-size: 50px;})");
 
-   layout()->addWidget(new SshToggle());
+  layout()->addWidget(new ParamControl("IsOpenpilotViewEnabled",
+                                       "오픈파일럿 주행화면 미리보기",
+                                       "오픈파일럿 주행화면을 미리보기 합니다.",
+                                       "../assets/offroad/icon_openpilot.png"
+                                       ));
+
+   layout()->addWidget(new AutoResumeToggle());
+   layout()->addWidget(new LiveSteerRatioToggle());
+   layout()->addWidget(new TurnSteeringDisableToggle());
+   layout()->addWidget(new PrebuiltToggle());
 }
 
 void UserPanel::showEvent(QShowEvent *event) 
 {
   Params params = Params();
 
-  /*
-  std::string brand = params.read_db_bool("Passive") ? "dashcam" : "openpilot";
-  QList<QPair<QString, std::string>> dev_params = {
-    {"Version", brand + " v" + params.get("Version", false).substr(0, 14)},
-    {"Git Branch", params.get("GitBranch", false)},
-    {"Git Commit", params.get("GitCommit", false).substr(0, 10)},
-    {"Panda Firmware", params.get("PandaFirmwareHex", false)},
-    {"OS Version", Hardware::get_os_version()},
-  };
 
-  for (int i = 0; i < dev_params.size(); i++) {
-    const auto &[name, value] = dev_params[i];
-    QString val = QString::fromStdString(value).trimmed();
-    if (labels.size() > i) {
-      labels[i]->setText(val);
-    } else {
-      labels.push_back(new LabelControl(name, val));
-      layout()->addWidget(labels[i]);
-      if (i < (dev_params.size() - 1)) {
-        layout()->addWidget(horizontal_line());
-      }
-    }
-  }
-
-*/
 }
 
 /*
@@ -144,5 +93,43 @@ QWidget * user_panel(QWidget * parent)
   w->setLayout(layout);
 
   return w;
+}
+*/
+
+
+/*
+UserPanel::UserPanel(QWidget* parent) : QFrame(parent)
+{
+  QVBoxLayout *layout = new QVBoxLayout;
+  layout->setMargin(100);
+  layout->setSpacing(30);
+
+  // simple wifi + tethering buttons
+  const char* launch_wifi = "am start -n com.android.settings/.wifi.WifiPickerActivity \
+                             -a android.net.wifi.PICK_WIFI_NETWORK \
+                             --ez extra_prefs_show_button_bar true \
+                             --es extra_prefs_set_next_text ''";
+  layout->addWidget(new ButtonControl("WiFi Settings", "OPEN", "",
+                                      [=]() { std::system(launch_wifi); }));
+
+  layout->addWidget(horizontal_line());
+
+  const char* launch_tethering = "am start -n com.android.settings/.TetherSettings \
+                                  --ez extra_prefs_show_button_bar true \
+                                  --es extra_prefs_set_next_text ''";
+  layout->addWidget(new ButtonControl("Tethering Settings", "OPEN", "",
+                                      [=]() { std::system(launch_tethering); }));
+
+  layout->addWidget(horizontal_line());
+
+  // SSH key management
+  layout->addWidget(new SshToggle());
+  layout->addWidget(horizontal_line());
+  layout->addWidget(new SshControl());
+
+  layout->addStretch(1);
+
+  QWidget *w = new QWidget;
+  w->setLayout(layout);
 }
 */
--- selfdrive/ui/qt/atom/userPanel.hpp
+++ selfdrive/ui/qt/atom/userPanel.hpp
@@ -20,20 +20,22 @@ class UserPanel : public QFrame
 public:
   explicit UserPanel(QWidget* parent = nullptr);
 
+private:
+   
 protected:
   void showEvent(QShowEvent *event) override;
-  QList<LabelControl *> labels;
+ // QList<LabelControl *> labels;
 };
 
 
 
-/*
-class AutoResumeToggle : public ToggleControl {
+
+class CAutoResumeToggle : public ToggleControl {
   Q_OBJECT
 
 public:
-  AutoResumeToggle() : ToggleControl("자동출발 기능 사용", "SCC 사용중 정차시 자동출발 기능을 사용합니다.", "../assets/offroad/icon_shell.png", Params().read_db_bool("OpkrAutoResume")) {
-    QObject::connect(this, &AutoResumeToggle::toggleFlipped, [=](int state) {
+  CAutoResumeToggle() : ToggleControl("자동출발 기능 사용", "SCC 사용중 정차시 자동출발 기능을 사용합니다.", "../assets/offroad/icon_shell.png", Params().read_db_bool("OpkrAutoResume")) {
+    QObject::connect(this, &CAutoResumeToggle::toggleFlipped, [=](int state) {
       char value = state ? '1' : '0';
       Params().write_db_value("OpkrAutoResume", &value, 1);
     });
@@ -41,12 +43,12 @@ public:
 };
 
 
-class LiveSteerRatioToggle : public ToggleControl {
+class CLiveSteerRatioToggle : public ToggleControl {
   Q_OBJECT
 
 public:
-  LiveSteerRatioToggle() : ToggleControl("Live SteerRatio 사용", "가변/고정 SR 대신 Live SteerRatio를 사용합니다.", "../assets/offroad/icon_shell.png", Params().read_db_bool("OpkrLiveSteerRatio")) {
-    QObject::connect(this, &LiveSteerRatioToggle::toggleFlipped, [=](int state) {
+  CLiveSteerRatioToggle() : ToggleControl("Live SteerRatio 사용", "가변/고정 SR 대신 Live SteerRatio를 사용합니다.", "../assets/offroad/icon_shell.png", Params().read_db_bool("OpkrLiveSteerRatio")) {
+    QObject::connect(this, &CLiveSteerRatioToggle::toggleFlipped, [=](int state) {
       char value = state ? '1' : '0';
       Params().write_db_value("OpkrLiveSteerRatio", &value, 1);
     });
@@ -54,12 +56,12 @@ public:
 };
 
 
-class TurnSteeringDisableToggle : public ToggleControl {
+class CTurnSteeringDisableToggle : public ToggleControl {
   Q_OBJECT
 
 public:
-  TurnSteeringDisableToggle() : ToggleControl("턴시그널 사용시 조향해제 사용", "차선변경속도 이하로 주행할 때 턴시그널을 사용시 자동조향을 일시해제 합니다.", "../assets/offroad/icon_shell.png", Params().read_db_bool("OpkrTurnSteeringDisable")) {
-    QObject::connect(this, &TurnSteeringDisableToggle::toggleFlipped, [=](int state) {
+  CTurnSteeringDisableToggle() : ToggleControl("턴시그널 사용시 조향해제 사용", "차선변경속도 이하로 주행할 때 턴시그널을 사용시 자동조향을 일시해제 합니다.", "../assets/offroad/icon_shell.png", Params().read_db_bool("OpkrTurnSteeringDisable")) {
+    QObject::connect(this, &CTurnSteeringDisableToggle::toggleFlipped, [=](int state) {
       char value = state ? '1' : '0';
       Params().write_db_value("OpkrTurnSteeringDisable", &value, 1);
     });
@@ -67,15 +69,14 @@ public:
 };
 
 
-class PrebuiltToggle : public ToggleControl {
+class CPrebuiltToggle : public ToggleControl {
   Q_OBJECT
 
 public:
-  PrebuiltToggle() : ToggleControl("Prebuilt 파일 생성", "Prebuilt 파일을 생성하며 부팅속도를 단축시킵니다. UI수정을 한 경우 기능을 끄십시오.", "../assets/offroad/icon_shell.png", Params().read_db_bool("PutPrebuiltOn")) {
-    QObject::connect(this, &PrebuiltToggle::toggleFlipped, [=](int state) {
+  CPrebuiltToggle() : ToggleControl("Prebuilt 파일 생성", "Prebuilt 파일을 생성하며 부팅속도를 단축시킵니다. UI수정을 한 경우 기능을 끄십시오.", "../assets/offroad/icon_shell.png", Params().read_db_bool("PutPrebuiltOn")) {
+    QObject::connect(this, &CPrebuiltToggle::toggleFlipped, [=](int state) {
       char value = state ? '1' : '0';
       Params().write_db_value("PutPrebuiltOn", &value, 1);
     });
   }
 };
-*/
