bdbe48f1509d965a58dfde67222c37bcff60182b
hikee9123 <hikee9123@gmail.com>
2021-04-03 15:35:22 +0900

    dd

--- selfdrive/ui/dashcam.h
+++ selfdrive/ui/dashcam.h
@@ -459,7 +459,7 @@ static void ui_draw_debug(UIState *s)
     ui_print( s, x_pos, y_pos+50,  "aO:%.2f, %.2f", angleOffset, angleOffsetAverage );
     ui_print( s, x_pos, y_pos+100, "sF:%.2f Fan:%.0f", stiffnessFactor, fanSpeed/1000. );
     ui_print( s, x_pos, y_pos+150, "lW:%.2f CV:%.0f", laneWidth, modelSpeed );
-    ui_print( s, x_pos, y_pos+200, "time:%d", scene.scr.nTime );
+    ui_print( s, x_pos, y_pos+200, "time:%d", scene.scr.nTime/15 );
 
 
     ui_print( s, x_pos, y_pos+250, "prob:%.2f, %.2f, %.2f, %.2f", lane_line_probs[0], lane_line_probs[1], lane_line_probs[2], lane_line_probs[3] );
