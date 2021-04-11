dcceb5cf489bbb4b9d4317dbc34bdb88938c737b
hikee9123 <hikee9123@gmail.com>
2021-04-11 18:36:50 +0900

    dd

--- selfdrive/car/tests/test_car_interfaces.py
+++ selfdrive/car/tests/test_car_interfaces.py
@@ -51,15 +51,15 @@ class TestCarInterfaces(unittest.TestCase):
       CC = car.CarControl.new_message()
       for _ in range(10):
         car_interface.update(CC, [])
-        car_interface.apply(CC, self.sm [])
-        car_interface.apply(CC, self.sm, [])
+        car_interface.apply(CC, self.sm, car_params)
+        car_interface.apply(CC, self.sm, car_params)
 
       CC = car.CarControl.new_message()
       CC.enabled = True
       for _ in range(10):
         car_interface.update(CC, [])
-        car_interface.apply(CC, self.sm, [])
-        car_interface.apply(CC, self.sm, [])
+        car_interface.apply(CC, self.sm, car_params)
+        car_interface.apply(CC, self.sm, car_params)
 
       # Test radar interface
       RadarInterface = importlib.import_module('selfdrive.car.%s.radar_interface' % car_params.carName).RadarInterface
