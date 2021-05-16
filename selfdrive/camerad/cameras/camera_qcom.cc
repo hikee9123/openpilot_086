f8ab3a8b34fe6847b2bb7a0bb6f4ab72d0835a4f
hikee9123 <hikee9123@gmail.com>
2021-05-16 10:54:40 +0900

    dd

--- selfdrive/camerad/cameras/camera_qcom.cc
+++ selfdrive/camerad/cameras/camera_qcom.cc
@@ -885,7 +885,7 @@ static void do_autofocus(CameraState *s, SubMaster *sm) {
   for (int i = 0; i < NUM_FOCUS; i++) pdebug += sprintf(pdebug, "%2x(%4d) ", s->confidence[i], s->focus[i]);
   pdebug += sprintf(pdebug, "  lens_true_pos: %7.2f   sag: %6.2f  cur_lens_pos: %4d->%4d \n", lens_true_pos, sag,  s->cur_lens_pos, target);
   //LOGD(debug);
-  print(debug);
+  printf(debug);
   actuator_move(s, target);
 }
 
