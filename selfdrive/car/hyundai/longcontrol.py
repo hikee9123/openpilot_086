8e72300b1c4fec871f86670c7edc388191d94629
hikee9123 <77569237+hikee9123@users.noreply.github.com>
2021-05-23 18:22:55 +0900

    Update longcontrol.py

--- selfdrive/car/hyundai/longcontrol.py
+++ selfdrive/car/hyundai/longcontrol.py
@@ -31,6 +31,7 @@ class CLongControl():
     self.curise_set_first = 0
     self.curise_sw_check = False
     self.cruise_set_mode = 4
+    self.cruise_set_speed_kph = 30
 
   def reset( self, CS ):
     self.scc12_cnt = CS.scc12["CR_VSM_Alive"] + 1     
