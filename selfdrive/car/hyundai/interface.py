fc60f62b4c3e110afb0bf8d8fd31e27ab05baebd
hikee9123 <77569237+hikee9123@users.noreply.github.com>
2021-05-31 15:51:23 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -41,7 +41,7 @@ class CarInterface(CarInterfaceBase):
     ret.communityFeature = candidate not in [CAR.SONATA, CAR.PALISADE]
 
     ret.steerActuatorDelay = 0.1  # Default delay
-    ret.steerRateCost = 0.5
+    ret.steerRateCost = 0.6
     ret.steerLimitTimer = 0.4
     tire_stiffness_factor = 1.
 
