df051fd17842c79e6e155b3a6582284d16950766
hikee9123 <hikee9123@gmail.com>
2021-06-06 08:07:26 +0900

    atompilot 0.8.5

--- selfdrive/ui/dashcam.h
+++ selfdrive/ui/dashcam.h
@@ -237,7 +237,7 @@ void draw_date_time(UIState *s)
 
   nvgFontSize(s->vg, 40*fFontSize);
   nvgFontFace(s->vg, "sans-semibold");
- // nvgFillColor(s->vg, nvgRGBA(255, 255, 255, 200));
+  nvgFillColor(s->vg, nvgRGBA(255, 255, 255, 200));
   nvgText(s->vg, 1602, 23, now, NULL);
 }
 
