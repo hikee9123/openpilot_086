5ea1343d52a8f0a8e644390a8dc7e74730c15917
hikee9123 <hikee9123@gmail.com>
2021-06-27 00:15:57 +0900

    dd

--- selfdrive/mapd/mapd.py
+++ selfdrive/mapd/mapd.py
@@ -58,80 +58,83 @@ class MapsdThread(threading.Thread):
             else:
               start = time.time()
 
-
             self.second += 0.25
             if self.second > 1.0:
               self.map_enabled = self.params.get_bool("OpkrMapEnable")
               self.second = 0.0
 
-            if self.map_enabled:
-                self.target_speed_map_counter += 1
-                if self.target_speed_map_counter >= (50+self.target_speed_map_counter1) and self.target_speed_map_counter_check == False:
-                    self.target_speed_map_counter_check = True
-                    os.system("logcat -d -s opkrspdlimit,opkrspd2limit | grep opkrspd | tail -n 1 | awk \'{print $7}\' > /data/params/d/LimitSetSpeedCamera &")
-                    os.system("logcat -d -s opkrspddist,opkrspd2dist | grep opkrspd | tail -n 1 | awk \'{print $7}\' > /data/params/d/LimitSetSpeedCameraDist &")
-                    os.system("logcat -d -s opkrsigntype,opkrspdsign | grep opkrspd | tail -n 1 | awk \'{print $7}\' > /data/params/d/OpkrMapSign &")
-                    self.target_speed_map_counter3 += 1
-                    if self.target_speed_map_counter3 > 2:
-                        self.target_speed_map_counter3 = 0
-                        os.system("logcat -c &")
-                elif self.target_speed_map_counter >= (75+self.target_speed_map_counter1):
-                    self.target_speed_map_counter1 = 0
-                    self.target_speed_map_counter = 0
-                    self.target_speed_map_counter_check = False
-                    try:
-                        mapspeed = self.params.get("LimitSetSpeedCamera", encoding="utf8")
-                        mapspeeddist = self.params.get("LimitSetSpeedCameraDist", encoding="utf8")
-                        mapsign = self.params.get("OpkrMapSign", encoding="utf8")
-                        if mapsign is not None:
+            if not self.map_enabled:
+              time.sleep(1.0)
+              continue
 
-                          try:
+
+            self.target_speed_map_counter += 1
+            if self.target_speed_map_counter >= (50+self.target_speed_map_counter1) and self.target_speed_map_counter_check == False:
+                self.target_speed_map_counter_check = True
+                os.system("logcat -d -s opkrspdlimit,opkrspd2limit | grep opkrspd | tail -n 1 | awk \'{print $7}\' > /data/params/d/LimitSetSpeedCamera &")
+                os.system("logcat -d -s opkrspddist,opkrspd2dist | grep opkrspd | tail -n 1 | awk \'{print $7}\' > /data/params/d/LimitSetSpeedCameraDist &")
+                os.system("logcat -d -s opkrsigntype,opkrspdsign | grep opkrspd | tail -n 1 | awk \'{print $7}\' > /data/params/d/OpkrMapSign &")
+                self.target_speed_map_counter3 += 1
+                if self.target_speed_map_counter3 > 2:
+                    self.target_speed_map_counter3 = 0
+                    os.system("logcat -c &")
+            elif self.target_speed_map_counter >= (75+self.target_speed_map_counter1):
+                self.target_speed_map_counter1 = 0
+                self.target_speed_map_counter = 0
+                self.target_speed_map_counter_check = False
+                try:
+                    mapspeed = self.params.get("LimitSetSpeedCamera", encoding="utf8")
+                    mapspeeddist = self.params.get("LimitSetSpeedCameraDist", encoding="utf8")
+                    mapsign = self.params.get("OpkrMapSign", encoding="utf8")
+                    if mapsign is not None:
+
+                        try:
                             mapsign = int(float(mapsign.rstrip('\n')))
                             self.map_sign = mapsign
-                          except:
+                        except:
                             pass
 
-                        else:
-                          mapsign = 0
-                          self.map_sign = mapsign
+                    else:
+                        mapsign = 0
+                        self.map_sign = mapsign
 
-                        if mapspeed is not None and mapspeeddist is not None:
+                    if mapspeed is not None and mapspeeddist is not None:
 
-                          try:
+                        try:
                             mapspeed = int(float(mapspeed.rstrip('\n')))
                             mapspeeddist = int(float(mapspeeddist.rstrip('\n')))
-                          except:
+                        except:
                             pass
 
-                          if mapspeed > 29:
+                        if mapspeed > 29:
                             self.target_speed_map = mapspeed
                             self.target_speed_map_dist = mapspeeddist
                             if self.target_speed_map_dist > 1001:
                                 self.target_speed_map_block = True
                             self.target_speed_map_counter1 = 80
                             os.system("logcat -c &")
-                          else:
+                        else:
                             self.target_speed_map = 0
                             self.target_speed_map_dist = 0
                             self.target_speed_map_block = False
-                        elif mapspeed is None and mapspeeddist is None and self.target_speed_map_counter2 < 2:
-                          self.target_speed_map_counter2 += 1
-                          self.target_speed_map_counter = 51
-                          self.target_speed_map = 0
-                          self.target_speed_map_dist = 0
-                          self.target_speed_map_counter_check = True
-                          self.target_speed_map_block = False
-                          self.target_speed_map_sign = False
-                        else:
-                          self.target_speed_map_counter = 49
-                          self.target_speed_map_counter2 = 0
-                          self.target_speed_map = 0
-                          self.target_speed_map_dist = 0
-                          self.target_speed_map_counter_check = False
-                          self.target_speed_map_block = False
-                          self.target_speed_map_sign = False
-                    except:
-                        pass
+                    elif mapspeed is None and mapspeeddist is None and self.target_speed_map_counter2 < 2:
+                        self.target_speed_map_counter2 += 1
+                        self.target_speed_map_counter = 51
+                        self.target_speed_map = 0
+                        self.target_speed_map_dist = 0
+                        self.target_speed_map_counter_check = True
+                        self.target_speed_map_block = False
+                        self.target_speed_map_sign = False
+                    else:
+                        self.target_speed_map_counter = 49
+                        self.target_speed_map_counter2 = 0
+                        self.target_speed_map = 0
+                        self.target_speed_map_dist = 0
+                        self.target_speed_map_counter_check = False
+                        self.target_speed_map_block = False
+                        self.target_speed_map_sign = False
+                except:
+                    pass
 
 
             dat = messaging.new_message()
