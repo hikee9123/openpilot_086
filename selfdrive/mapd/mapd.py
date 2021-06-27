ff3be7782c90c9677ffcbeec4ef4597386e044f9
hikee9123 <hikee9123@gmail.com>
2021-06-27 18:06:31 +0900

    dd

--- selfdrive/mapd/mapd.py
+++ selfdrive/mapd/mapd.py
@@ -99,7 +99,7 @@ class MapsdThread(threading.Thread):
             print( "am start com.skt.tmap.ku/com.skt.tmap.activity.TmapNaviActivity" )
             os.system("am start com.skt.tmap.ku/com.skt.tmap.activity.TmapNaviActivity")
             if self.map_enabled == 2:  # Tmap 실행후 Overlay mode로 변경합니다.
-                self.target_speed_map_counter2 = 5
+                self.target_speed_map_counter2 = 3
 
 
 
