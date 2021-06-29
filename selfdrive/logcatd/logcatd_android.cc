0a6702c0ba1fc2c97d7a0beb0136f8cfe4a2761a
hikee9123 <hikee9123@gmail.com>
2021-06-29 12:45:59 +0900

    dd

--- selfdrive/logcatd/logcatd_android.cc
+++ selfdrive/logcatd/logcatd_android.cc
@@ -58,7 +58,7 @@ int main() {
 
 
 
-      printf("logcat - tag=[%s] \n",entry.tag);
+      printf("logcat ID(%d) - PID=%d tag=%d.[%s] \n", log_msg.id(), entry.pid, entry.tag);
       printf("entry.message=[%s]\n", entry.message);
 
       pm.send("androidLog", msg);
