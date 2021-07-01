8ffbbbcdacec6ef7aa83d1426992c78487171ae4
hikee9123 <77569237+hikee9123@users.noreply.github.com>
2021-07-02 07:44:32 +0900

    Update logcatd_android.cc

--- selfdrive/logcatd/logcatd_android.cc
+++ selfdrive/logcatd/logcatd_android.cc
@@ -113,12 +113,12 @@ int main() {
 
      
       
-      if( opkr )
-      {
+      //if( opkr )
+      //{
         printf("logcat ID(%d) - PID=%d tag=%d.[%s] \n", log_msg.id(), entry.pid,  entry.tid, entry.tag);
         printf("entry.message=[%s]\n", entry.message);
         printf("spd = %f\n", res.speedLimit );
-      }
+      //}
 
       pm.send("liveMapData", msg);
     }
